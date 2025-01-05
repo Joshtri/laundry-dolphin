6acfff551f385aa581e8510b79bcf6b989e12924
Joshtri <stuffofyos1516@gmail.com>
2025-01-05 20:22:04 +0800

    update homepage page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Layout from './Layout'
+import Hero from '../components/Home/Hero'
+import Services from '../components/Home/Services'
+import WhyChooseUs from '../components/Home/WhyChooseUs'
+import Testimonials from '../components/Home/Testimonials'
+import Location from '../components/Home/Location'
+import Pricelist from '../components/Home/PriceList'
+import ContactUs from '../components/Home/ContactUs'
+
+const HomePage = () => {
+  return (
+    <Layout>
+        {/* <main className="p-4">
+            <h1 className="text-2xl font-bold mb-4">Welcome to Dolphin Laundry</h1>
+            <p className="mb-4">The best laundry service in town!</p>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded">Join Now</button>
+        </main> */}
+
+      <Hero />
+      <Services />
+      <WhyChooseUs />
+      <Testimonials />
+      <Location />
+      <Pricelist/>
+      <ContactUs />
+
+      {/* <FeedbackForm /> */}
+    </Layout>
+  )
+}
+
+export default HomePage
