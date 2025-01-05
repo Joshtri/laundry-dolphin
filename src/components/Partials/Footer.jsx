1a34b27f0a8b29530322172e21621c04a3f0c45b
Joshtri <stuffofyos1516@gmail.com>
2025-01-05 20:21:22 +0800

    add footer comp

---
+++ src/components/Partials/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Mendapatkan tahun saat ini secara dinamis
+
+  return (
+    <footer className="bg-blue-500 text-white py-6">
+      <div className="container mx-auto px-4 text-center">
+        <p className="text-lg font-semibold">Dolphin Laundry & Dry Cleaning</p>
+        <p className="mt-2 text-sm">
+          © {currentYear} Dolphin Laundry. All rights reserved.
+        </p>
+        <div className="flex justify-center space-x-4 mt-4">
+          <a
+            href="#"
+            className="text-white hover:text-yellow-400 transition duration-300"
+          >
+            Privacy Policy
+          </a>
+          <span>|</span>
+          <a
+            href="#"
+            className="text-white hover:text-yellow-400 transition duration-300"
+          >
+            Terms of Service
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
