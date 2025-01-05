3722240ac828a3d2e8b3a7bf4f5c4bda2df3f00e
Joshtri <stuffofyos1516@gmail.com>
2025-01-05 21:33:24 +0800

    update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,37 +1,39 @@
-// import React from 'react'
-import Layout from './Layout'
-import Hero from '../components/Home/Hero'
-import Services from '../components/Home/Services'
-import WhyChooseUs from '../components/Home/WhyChooseUs'
-import Testimonials from '../components/Home/Testimonials'
-import Location from '../components/Home/Location'
-import Pricelist from '../components/Home/PriceList'
-import ContactUs from '../components/Home/ContactUs'
+import React from "react";
+import Layout from "./Layout";
+import Hero from "../components/Home/Hero";
+import Services from "../components/Home/Services";
+import WhyChooseUs from "../components/Home/WhyChooseUs";
+import Testimonials from "../components/Home/Testimonials";
+import Location from "../components/Home/Location";
+import Pricelist from "../components/Home/PriceList";
+import ContactUs from "../components/Home/ContactUs";
 
-/**
- * Renders the home page of the website.
- * @returns {JSX.Element} The home page component.
- */
 const HomePage = () => {
   return (
     <Layout>
-        {/* <main className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Welcome to Dolphin Laundry</h1>
-            <p className="mb-4">The best laundry service in town!</p>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded">Join Now</button>
-        </main> */}
-
-      <Hero />
-      <Services />
-      <WhyChooseUs />
-      <Testimonials />
-      <Location />
-      <Pricelist/>
-      <ContactUs />
-
-      {/* <FeedbackForm /> */}
+      <section id="home">
+        <Hero />
+      </section>
+      <section id="services">
+        <Services />
+      </section>
+      <section id="why-choose-us">
+        <WhyChooseUs />
+      </section>
+      <section id="testimonials">
+        <Testimonials />
+      </section>
+      <section id="location">
+        <Location />
+      </section>
+      <section id="pricelist">
+        <Pricelist />
+      </section>
+      <section id="contact-us">
+        <ContactUs />
+      </section>
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
