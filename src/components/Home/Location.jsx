ea1a5e0115632ada6f02cd50750f6ef9e2d24964
Joshtri <stuffofyos1516@gmail.com>
2025-01-07 19:17:22 +0800

    updati time infor

--- src/components/Home/Location.jsx
+++ src/components/Home/Location.jsx
@@ -57,7 +57,7 @@ const Location = () => {
               allowFullScreen=""
               loading="lazy"
             ></iframe>
-            <p className="mt-4 font-semibold">Jam Buka: 06.00 AM - 22.00 PM</p>
+            <p className="mt-4 font-semibold">Jam Buka: 05.45 AM - 22.00 PM</p>
             <button className="mt-4 bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600">
                 <a href="https://goo.gl/maps/Tu5ijHJKQZAwYQiA6" target="_blank">
                     Ke Lokasi Laundry
